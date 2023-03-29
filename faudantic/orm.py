7d408cf8140f8ae39fcd54ac1502346da3b208b1
Oscar Bahamonde <107950590+obahamonde@users.noreply.github.com>
2023-03-29 03:05:49 -0500

    async

--- faudantic/orm.py
+++ faudantic/orm.py
@@ -2,14 +2,16 @@
    the fauna query object is available also within the class for further customization
 """
 from __future__ import annotations
+import asyncio
 from os import environ
-from typing import List, Optional, Any, Dict, Generator, Callable
+from typing import List, Optional, Any, Callable, Generator
 from pydantic import BaseSettings, BaseConfig, Field
 from faunadb import query as q
 from faunadb.objects import Query
 from faunadb.client import FaunaClient
 from faunadb.errors import FaunaError
 from faudantic.json import JSONModel
+from faudantic.client import AsyncFaunaClient
 
 
 class Environment(BaseSettings):
@@ -28,7 +30,6 @@ class Environment(BaseSettings):
 
 
 env = Environment()
-
 
 class FaunaModel(JSONModel):
     """FaunaDB Model"""
@@ -231,6 +232,203 @@ class FaunaModel(JSONModel):
             for ref in refs:
                 data = cls.q()(q.get(ref))
                 yield cls(**{**data["data"], "ref": data["ref"].id(), "ts": data["ts"]})
+        except FaunaError as exc:
+            print(exc)
+            return None
+
+class AsyncFaunaModel(JSONModel):
+    """FaunaDB Model"""
+    ref: Optional[str] = None
+
+    @classmethod
+    def client(cls) -> AsyncFaunaClient:
+        """Return a FaunaClient"""
+        return AsyncFaunaClient(secret=env.fauna_secret)
+
+    @classmethod
+    def q(cls):
+        """Return a FaunaDB query"""
+        return cls.client().query
+
+    @classmethod
+    async def provision(cls) -> None:
+        """Provision the collection and indexes"""
+        _q = cls.q()
+        try:
+            if not await _q(q.exists(q.collection(cls.__name__.lower()))):
+                await _q(q.create_collection({"name": cls.__name__.lower()}))
+                print(f"Created collection {cls.__name__.lower()}")
+                await _q(
+                    q.create_index(
+                        {
+                            "name": cls.__name__.lower(),
+                            "source": q.collection(cls.__name__.lower()),
+                        }
+                    )
+                )
+                print(f"Created index {cls.__name__.lower()}")
+            for field in cls.__fields__.values():
+                if field.field_info.extra.get("unique"):
+                    await _q(
+                        q.create_index(
+                            {
+                                "name": f"{cls.__name__.lower()}_{field.name}_unique",
+                                "source": q.collection(cls.__name__.lower()),
+                                "terms": [{"field": ["data", field.name]}],
+                                "unique": True,
+                            }
+                        )
+                    )
+                    print(
+                        f"Created unique index {cls.__name__.lower()}_{field.name}_unique"
+                    )
+                    continue
+                if field.field_info.extra.get("index"):
+                    await _q(
+                        q.create_index(
+                            {
+                                "name": f"{cls.__name__.lower()}_{field.name}",
+                                "source": q.collection(cls.__name__.lower()),
+                                "terms": [{"field": ["data", field.name]}],
+                            }
+                        )
+                    )
+                    print(f"Created index {cls.__name__.lower()}_{field.name}")
+                    continue
+        except FaunaError as exc:
+            print(exc)
+
+    @classmethod
+    async def exists(cls, ref: str) -> Optional[bool]:
+        """Check if a document exists"""
+        try:
+            return await cls.q()(q.exists(q.ref(q.collection(cls.__name__.lower()), ref)))
+        except FaunaError as exc:
+            print(exc)
+            return None
+
+    @classmethod
+    async def find_unique(cls, field: str, value: Any) -> Optional[AsyncFaunaModel]:
+        """Find a document by a unique field"""
+        try:
+            data = await cls.q()(
+                q.get(q.match(q.index(f"{cls.__name__.lower()}_{field}_unique"), value))
+            )
+            return cls(**{**data["data"], "ref": data["ref"].id(), "ts": data["ts"]/1000})
+        except FaunaError as exc:
+            print(exc)
+            return None
+
+    @classmethod
+    async def find_many(cls, field: str, value: Any) -> Optional[List[AsyncFaunaModel]]:
+        """Find documents by a field"""
+        try:
+            _q = cls.q()
+            refs = (await _q(
+                q.paginate(q.match(q.index(f"{cls.__name__.lower()}_{field}"), value))
+            ))["data"]
+            data = await asyncio.gather(*[_q(q.get(ref)) for ref in refs])
+            return [
+                cls(**{**d["data"], "ref": d["ref"].id(), "ts": d["ts"]/1000}) for d in data
+            ]
+        except FaunaError as exc:
+            print(exc)
+            return None
+
+    @classmethod
+    async def find(cls, ref: str) -> Optional[AsyncFaunaModel]:
+        """Find a document by id"""
+        try:
+            data = await cls.q()(q.get(q.ref(q.collection(cls.__name__.lower()), ref)))
+            return cls(**{**data["data"], "ref": data["ref"].id(), "ts": data["ts"]/1000})
+        except FaunaError as exc:
+            print(exc)
+            return None
+
+    @classmethod
+    async def find_all(cls) -> Optional[List[AsyncFaunaModel]]:
+        """Find all documents"""
+        try:
+            _q = cls.q()
+            refs = (await _q(q.paginate(q.match(q.index(cls.__name__.lower())))))["data"]
+            data = await asyncio.gather(*[_q(q.get(ref)) for ref in refs])
+            return [
+                cls(**{**d["data"], "ref": d["ref"].id(), "ts": d["ts"]/1000}) for d in data
+            ]
+        except FaunaError as exc:
+            print(exc)
+            return None
+
+    @classmethod
+    async def delete_unique(cls, field: str, value: Any) -> bool:
+        """Delete a document by a unique field"""
+        try:
+            _q = cls.q()
+            ref = await _q(
+                q.get(q.match(q.index(f"{cls.__name__.lower()}_{field}_unique"), value))
+            )
+            await _q(q.delete(ref))
+            return True
+        except FaunaError:
+            return False
+
+    @classmethod
+    async def delete(cls, ref: str) -> bool:
+        """Delete a document by id"""
+        try:
+            await cls.q()(q.delete(q.ref(q.collection(cls.__name__.lower()), ref)))
+            return True
+        except FaunaError:
+            return False
+
+    async def create(self) -> Optional[AsyncFaunaModel]:
+        """Create a document"""
+        try:
+           
+            data = await self.q()(
+                q.create(
+                    q.collection(self.__class__.__name__.lower()), {"data": self.dict()}
+                ))
+            
+            return data
+
+        except FaunaError as exc:
+            print(exc)
+            return None
+
+    @classmethod
+    async def update(cls, ref: str, **kwargs) -> Optional[AsyncFaunaModel]:
+        """Update a document"""
+        try:
+            data = await cls.q()(
+                q.update(
+                    q.ref(q.collection(cls.__name__.lower()), ref), {"data": kwargs}
+                )
+            )
+            return cls(**{**data["data"], "ref": data["ref"].id(), "ts": data["ts"]})
+        except FaunaError as exc:
+            print(exc)
+            return None
+
+    async def upsert(self) -> Optional[AsyncFaunaModel]:
+        """Upsert a document"""
+        try:
+            if not self.ref:
+                return await self.create()
+            return await self.update(self.ref, **self.dict())
+        except FaunaError as exc:
+            print(exc)
+            return None
+
+    @classmethod
+    async def query(cls, query: str) -> Optional[List[AsyncFaunaModel]]:
+        """Run a query"""
+        try:
+            refs = (await cls.q()(q.paginate(q.match(q.query(query)))))["data"]
+            data = await asyncio.gather(*[cls.q()(q.get(ref)) for ref in refs])
+            return [
+                cls(**{**d["data"], "ref": d["ref"].id(), "ts": d["ts"]/1000}) for d in data
+            ]
         except FaunaError as exc:
             print(exc)
             return None
